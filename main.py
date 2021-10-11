d7574841edc81c2ff191c23107df0581e75225b3
Nano <68131578+DarkRabbit-0@users.noreply.github.com>
2021-10-10 19:03:55 -0500

    fixed alot
    10x better

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
 '''
 ----------------------------------------------
 Python Site Backend Finder  - Quey Development
-
 Creators: NeonTheDev, Cuts, Timanttikuutio
-
 Collaborators: NEXUS#7496
-
 Contact: Cuts#0001, Neon#2928 (neon@neonthe.dev), Timanttikuutio#0001, NEXUS#7496
-
 Please give credits if this code is used.
-
 Streamed live on https://twitch.tv/QueyDev
+
+Updated by github.com/psauxx/ <3 
 ----------------------------------------------
 '''
 
 # Imports
-import json
-import os
-import requests
-import random
-import time
-import threading
-import discord
-import socket
+import requests, sys, random, threading, discord, socket, selenium, json
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-with open("files/config.json", "r") as fp:
+with open("config.json", "r") as fp:
     config = json.loads(fp.read())
 
-if '-d' in sys.argv: debug = True
-else: debug = False
+debug = True
 
 def checkip():
     badcodes = []
@@ -39,45 +29,42 @@ def checkip():
     while 1:
         try:
             ip = f"{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}"
-            with open("files/ips.txt", "a+") as fp:
+            with open("Results/ips.txt", "a+") as fp:
                 fp.write(ip + "\n")
-
-            try:
-                #check = requests.get(f'http://{ip}:80', timeout=1)  # Scan the IP for a HTTP conection.
-                check = sock.connect_ex((ip, 80))
-                if debug: print(f'HIT! IP: http://{ip}') # MIGHT CRASH
-            except requests.exceptions.ConnectionError:  # If the connection is unavailable, pass.
-                #check = requests.get(f'https://{ip}:80', timeout=1)
-                check = sock.connect_ex((ip, 443))
-                if debug: print(f'HIT! IP: https://{ip}') # MIGHT CRASH
-
-            #if str(check.status_code) not in badcodes:  # Checking if its a good response code.
-            if check == 0:
-                ipinfo(ip)  # Create the embed by calling the ipinfo function.
-
-        except requests.exceptions.ConnectionError:  # If the connection is unavailable, pass.
-            pass
-
-        except KeyboardInterrupt:
-            sys.exit()
-
-        except Exception as e:  #
-            print(e)
+                with open('Results/ip_working.txt', "a+") as ff:
+                    try:
+                        check = sock.connect_ex((ip, 80))
+                        port = 80
+                    except:  
+                        check = sock.connect_ex((ip, 443))
+                        port = 443
+                    if check == 0:
+                        print(f"HIT! http://{ip}:{port}")
+                        ff.write(ip+'\n')
+                        ipinfo(ip, port)
+                        
+                    
+        except (requests.exceptions.ConnectionError, KeyboardInterrupt):  #
+            if requests.exceptions.ConnectionError:
+                pass
+            elif KeyboardInterrupt:
+                exit(0)
             pass
 
 def send_screenshot(url):
     chromeopts = Options()
     chromeopts.add_argument("--headless")
-    # chromeopts.add_argument("--window_size=1920,1080")
+    chromeopts.add_experimental_option('excludeSwitches', ['enable-logging'])
     chromeopts.headless = True
 
-    driver = webdriver.Chrome("files/chromedriver.exe", options=chromeopts)
+    driver = webdriver.Chrome("chromedriver.exe", options=chromeopts)
     driver.get(url)
     driver.save_screenshot("screenshot.png")
 
 
-def ipinfo(ip):
-    blockedisps = ["Akamai Technologies, Inc.", "Cloudflare, Inc.", "Amazon Technologies Inc.", "Akamai"]
+def ipinfo(ip, port):
+    blockedisps = ["Akamai Technologies, Inc.", "Cloudflare, Inc.", "Amazon Technologies Inc.", "Akamai", "cloudfront", "ERROR: The request could not be satisfied"]
+    blocks = ["Invalid", "503"]
     geo = requests.get(f'http://extreme-ip-lookup.com/json/{ip}').json()
     ipaddr = f"[{geo['query']}](https://{geo['query']})"
     if geo.get("isp") in blockedisps:
@@ -86,36 +73,49 @@ def ipinfo(ip):
     else:
         try:
             send_screenshot(f"http://{ip}")
-
-            webhook = discord.Webhook.from_url(random.choice(config["webhooks"]), adapter=discord.RequestsWebhookAdapter(requests.session()))
+            if port == 443:
+                _url = requests.get('https://'+ip).text
+            else:
+                _url = requests.get('http://'+ip).text
+            soup = BeautifulSoup(_url, 'html.parser')
+            _tit = "Not found"
+            for title in soup.find_all('title'):
+                _tit = title.get_text()
+            if _tit in blockedisps:
+                print("Blocked ISP: " + geo.get("isp"))
+                pass
+            if _url in blocks:
+                print("Blocked: "+ip)
+                pass
+            webhook = discord.Webhook.from_url(config['webhooks'], adapter=discord.RequestsWebhookAdapter(requests.session()))
             webhook.send(
                 file=discord.File("screenshot.png", filename="screenshot.png"),
                 embed=discord.Embed.from_dict({
-                    "title": f"SITE SCANNER: {ip}",
+                    "title": f"Site Finder",
                     "description": "Site found on this IP.",
                     "fields": [
-                        {'name': 'IP', 'value': ipaddr},
+                        {'name': 'IP', 'value': ipaddr+f":{port}"},
                         {'name': 'IP Type', 'value': geo.get("ipType")},
+                        {'name': 'Website Title', 'value': _tit},
                         {'name': 'Country', 'value': geo.get("country")},
-                        {'name': 'City', 'value': geo.get("city")},
-                        {'name': 'Continent', 'value': geo.get("continent")},
-                        {'name': 'Country', 'value': geo.get("country")},
-                        {'name': 'Region', 'value': geo.get("region")},
                         {'name': 'ISP', 'value': geo.get("isp")},
                         {'name': 'Org', 'value': geo.get("org")}
                     ],
                     "image": {"url": "attachment://screenshot.png"},
                     "footer": {
                         "text": "Quey development",
-                    }
+                    },
+                    "color": 1127128
                 })
             )
+            
+
+            
         except:
             pass
 
 
 def main():
-    print('Thread started!')
     checkip()
 
 if __name__ == '__main__':
@@ -125,4 +125,5 @@ if __name__ == '__main__':
         t = threading.Thread(target=main, daemon=True)
         threads.append(t)
         t.start()
-    input('Scanning.')
+        
+    input('Done Scanning!\n')
